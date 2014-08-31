2b8f6a0027698cf60cf36ce917251107d0b1a076
alvarokb2 <pastalvaro@gmail.com>
2014-08-31 17:10:39 -0400

    tarea01 catualizacion 1

---
+++ tarea01/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Alvaro
+ *
+ * Created on 31 de agosto de 2014, 17:08
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
