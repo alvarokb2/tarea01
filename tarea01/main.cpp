d8a36b41c7e23c20302110bf1eac314309fbb214
hgjfkdls <fcabedov@gmail.com>
2014-08-31 18:17:49 +0100

    Actualizacion Pipe 01

--- tarea01/main.cpp
+++ tarea01/main.cpp
@@ -15,40 +15,47 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    if (*(argv + 1) != NULL) {
-        if (strcmp(*(argv + 1), "-g") == 0) // si el primer argumento es -g
-        {
-            if (*(argv + 2) != NULL
-                    && *(argv + 3) != NULL
-                    && *(argv + 4) != NULL
-                    && *(argv + 5) == NULL) //verifica que se pasen 4 argumentos
-            {
-                // Pipe:ingrese esta linea
-                
-                ifstream flujo(*(argv + 4));
-
-                flujo.close();
-
-            } else //si el primer parametro es -g pero no tiene 4 parametros
-            {
-                cout << "Error al ingresar parametros" << endl;
-                cout << "el formato que debe utilizar es" << endl;
-                cout << "-g yyyy-mm-dd yyyy-mm-dd archivo.csv" << endl;
-            }
-
-        } else if (strcmp(*(argv + 1), "-v") == 0) // si el primer argumento es -v
-        {
-            if (*(argv + 2) != NULL) //si tiene mas de 1 argumento
-            {
-                cout << "Para mostrar los integrantes" << endl;
-                cout << "y fecha de compilacion debe ingresar" << endl;
-                cout << "el parametro -v como unico parametro" << endl;
-            } else {
-                cout << "Integrantes del Grupo";
-            }
-        }
-    } else //al no pasarle parametros el programa no hace nada
+    system("cls");
+    cout << "- Tarea01 -\n\n -Contador de argumentos: "<< argc << endl;
+    
+    if(argc==2)
     {
-        return 0;
+        if(strcmp(*(argv+1),"-v")==0)
+        {
+            cout << "\nIntegrantes del Grupo";
+        }
+        else
+        {
+            cout << "\nError: Si ingresa 1 argumento debe ser -v";
+        }
     }
+    else if(argc==5)
+    {
+        if(strcmp(*(argv+1),"-g")==0)
+        {
+            cout << "\n Exito";
+            
+            ifstream flujo(*(argv + 4));
+                
+            char c=flujo.get();
+            while(flujo.good())
+            {
+                cout << c;
+                c = flujo.get();
+            }
+            flujo.close();
+            
+        }
+        else
+        {
+            cout <<"\nError: wefefewfef";
+        }
+    }
+    else
+    {
+        cout << "\nError: Debe ingresar parametros\n  - prueba [-v | -g <yyyy-mm-dd> <yyyy-mm-dd> <archivo>.csv ]";
+    }
+    cout << "\n\n- End -\n";
+    
+    return 0;
 }
