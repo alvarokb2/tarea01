55d3e06c9422786d5b391ca9d79daa58f29eb305
alvarokb2 <pastalvaro@gmail.com>
2014-08-31 19:16:09 -0400

    tarea01 actualizacion 3

--- tarea01/main.cpp
+++ tarea01/main.cpp
@@ -15,6 +15,27 @@
 
 using namespace std;
 
+
+void ValidarArgumentos(char fechaInicio[32])
+{
+    string fechai = fechaInicio;
+    string aux;
+    if(fechai.length()==10 && fechai.substr(4,1)=="-" && fechai.substr(7,1)=="-")
+    {
+        aux = fechai.substr(0,4)+fechai.substr(5,2)+fechai.substr(8,2);
+        fechai = aux.substr(0,8 );
+        cout <<fechai;
+        
+        for(int i=0;i<fechai.length();i++)
+        {
+            while(isdigit(fechai[i]))
+            {
+                    
+            }
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     system("cls");
     cout << "- Tarea01 -\n\n -Contador de argumentos: "<< argc << endl;
