f6112b06349375e32900c40aa36312b4c17900d6
hgjfkdls <fcabedov@gmail.com>
2014-08-31 21:54:52 +0100

    ok

--- tarea01/main.cpp
+++ tarea01/main.cpp
@@ -19,28 +19,25 @@ bool ValidarFecha(char fechaInicio[32]) {
     string aux;
     if (fechai.length() == 10) {
         if (fechai.substr(4, 1) == "-" && fechai.substr(7, 1) == "-") {
-
             aux = fechai.substr(0, 4) + fechai.substr(5, 2) + fechai.substr(8, 2);
             fechai = aux.substr(0, 8);
-
             for (int i = 0; i < fechai.length(); i++) {
                 if (!isdigit(fechai[i])) {
-
-                    cout << "Las fechas solo pueden contener numeros y guiones en formato yyyy-mm-dd ";
+                    cout << "  - Las fechas solo pueden contener numeros y guiones en formato yyyy-mm-dd " << endl;
                     return false;
                 }
             }
-            cout << "fecha ingresada correctamente";
+            cout << "  - fecha ingresada correctamente" << endl;
             return true;
         }
         else{
-            cout << "Las fechas solo pueden contener numeros y guiones en formato yyyy-mm-dd ";
+            cout << "  - Las fechas solo pueden contener numeros y guiones en formato yyyy-mm-dd " << endl;
             return false;
         }
     }
     else
     {
-        cout << "Las fechas solo pueden contener numeros y guiones en formato yyyy-mm-dd ";
+        cout << "  - Las fechas solo pueden contener numeros y guiones en formato yyyy-mm-dd " << endl;
         return false;
     }
 }
@@ -50,18 +47,19 @@ bool ValidarArchivo(char archivo[64]){
     
     if(file.substr(file.length()-4)==".csv")
     {
-        cout <<"formato correcto";
+        cout << "  - formato correcto" << endl;
         return true;
     }
     else
     {
-        cout <<"formato incorrecto";
+        cout << "  - formato incorrecto" << endl;
         return false;
     }
 }
 
 bool ValidarArgumentos(char f1[32],char f2[32], char archivo[64])
 {
+    cout << endl << "Validar argumentos :" << endl;
     if(ValidarFecha(f1) && ValidarFecha(f2) && ValidarArchivo(archivo)){
         return true;
     }
@@ -77,13 +75,12 @@ int main(int argc, char **argv) {
 
     if (argc == 2) {
         if (strcmp(*(argv + 1), "-v") == 0) {
-            cout << "\nIntegrantes del Grupo";
+            cout << "Integrantes del Grupo" << endl;
         } else {
-            cout << "\nError: Si ingresa 1 argumento debe ser -v";
+            cout << "Error: Si ingresa 1 argumento debe ser -v" << endl;
         }
     } else if (argc == 5) {
         if (strcmp(*(argv + 1), "-g") == 0) {
-            cout << "\n Exito";
 
             ValidarArgumentos(*(argv + 2),*(argv + 3),*(argv + 4));
                 
@@ -96,18 +93,18 @@ int main(int argc, char **argv) {
                 str.append(&c);
                 c = flujo.get();
             }
-            cout << "Archivo: " << *(argv+4) << endl;
+            cout << endl << "Archivo: " << *(argv+4) << endl;
             cout << "  - Size  : " << str.size() << endl;
             cout << "  - Lines : " << count << endl;
             flujo.close();
 
         } else {
-            cout << "\nError: wefefewfef";
+            cout << "Error: Debe ingresar parametros\n  - prueba [-v | -g <yyyy-mm-dd> <yyyy-mm-dd> <archivo>.csv ]" << endl;
         }
     } else {
-        cout << "\nError: Debe ingresar parametros\n  - prueba [-v | -g <yyyy-mm-dd> <yyyy-mm-dd> <archivo>.csv ]";
+        cout << "Error: Debe ingresar parametros\n  - prueba [-v | -g <yyyy-mm-dd> <yyyy-mm-dd> <archivo>.csv ]" << endl;
     }
-    cout << "\n\n- End -\n";
+    cout << "\n- End -\n";
 
     return 0;
 }
