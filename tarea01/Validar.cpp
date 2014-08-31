7fc4eae33efd2b9bd162d8b2716abe0d73f0de6b
alvarokb2 <pastalvaro@gmail.com>
2014-08-31 19:04:14 -0400

    tarea01 actualizacion 3

--- tarea01/Validar.cpp
+++ tarea01/Validar.cpp
@@ -11,11 +11,18 @@ void ValidarArgumentos(char fechaInicio[32])
 {
     string fechai = fechaInicio;
     string aux;
-
     if(fechai.length()==10 && fechai.substr(4,1)=="-" && fechai.substr(7,1)=="-")
     {
         aux = fechai.substr(0,4)+fechai.substr(5,2)+fechai.substr(8,2);
         fechai = aux.substr(0,8 );
         cout <<fechai;
+        
+        for(int i=0;i<fechai.length();i++)
+        {
+            while(isdigit(fechai[i]))
+            {
+                    
+            }
+        }
     }
 }
