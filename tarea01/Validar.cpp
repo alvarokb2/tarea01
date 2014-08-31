9e4fcd06697dabc77457b8faf70e8909dc370517
alvarokb2 <pastalvaro@gmail.com>
2014-08-31 18:25:03 -0400

    tarea01 actualizacion 2

---
+++ tarea01/Validar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+bool ValidarArgumentos(char fechaInicio[128], char fechaTermino[], char archivo[128])
+{
+    string fechai = fechaInicio;
+    string fechat = fechaTermino;
+    string file = archivo;
+
+    if(fechai.length()==10)
+    {
+        
+    }
+}
--- tarea01/nbproject/Makefile-Release.mk
+++ tarea01/nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/Validar.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tarea01.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tarea01 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/Validar.o: Validar.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Validar.o Validar.cpp
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
