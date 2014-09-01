a427083754e2b27bad0ce54351d84994d37a6528
alvarokb2 <pastalvaro@gmail.com>
2014-08-31 21:44:52 -0400

    tarea01 actualizacion 7

--- tarea01/main.cpp
+++ tarea01/main.cpp
@@ -98,41 +98,6 @@ bool ValidarArgumentos(char f1[32],char f2[32], char archivo[64])
         return false;
     }
 }
-int main(int argc, char **argv) {
-    system("cls");
-    cout << "- Tarea01 -\n\n -Contador de argumentos: " << argc << endl;
-
-    if (argc == 2) {
-        if (strcmp(*(argv + 1), "-v") == 0) {
-            cout << "\nIntegrantes del Grupo";
-        } else {
-            cout << "\nError: Si ingresa 1 argumento debe ser -v";
-        }
-    } else if (argc == 5) {
-        if (strcmp(*(argv + 1), "-g") == 0) {
-            cout << "\n Exito";
-
-            ValidarArgumentos(*(argv + 2),*(argv + 3),*(argv + 4));
-                
-            ifstream flujo(*(argv + 4));
-
-            char c = flujo.get();
-            while (flujo.good()) {
-                cout << c;
-                c = flujo.get();
-            }
-            flujo.close();
-
-        } else {
-            cout << "\nError: wefefewfef";
-        }
-    } else {
-        cout << "\nError: Debe ingresar parametros\n  - prueba [-v | -g <yyyy-mm-dd> <yyyy-mm-dd> <archivo>.csv ]";
-    }
-    cout << "\n\n- End -\n";
-
-    return 0;
-}
 
 int main(int argc, char **argv) {
     system("cls");
